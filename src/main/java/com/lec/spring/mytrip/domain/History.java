8793629fcc7562f6bba51bbb0edecbf1b4adefec
kp-of-s <gimbhs@gmail.com>
2024-12-02 23:54:13 +0900

    feat: 변수명 변경

--- src/main/java/com/lec/spring/mytrip/domain/History.java
+++ src/main/java/com/lec/spring/mytrip/domain/History.java
@@ -15,9 +15,9 @@ public class History {
 
     private User user; // 검색한자(FK)
 
-    private int search_history_id; // 검색 기록 테이블 ID
-    private String qry_url; // 검색 기록 쿼리
-    private String start_name; // 출발지
-    private String end_name; // 도착지
+    private int searchHistoryId; // 검색 기록 테이블 ID
+    private String qryUrl; // 검색 기록 쿼리
+    private String startName; // 출발지
+    private String endName; // 도착지
 
 } // end History class
