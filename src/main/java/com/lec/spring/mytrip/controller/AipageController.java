b947fa567bc17e2b68d1e564c734510cbd587f87
0105moonsun <0105moonsun@gmail.com>
2024-12-03 11:32:10 +0900

    feat: AI 변경 내용 (취리히-> 델리, 바르셀루나 -> 하얼빈)

---
+++ src/main/java/com/lec/spring/mytrip/controller/AipageController.java
@@ -0,0 +1,20 @@
+package com.lec.spring.mytrip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/aipage")
+public class AipageController {
+
+    // TODO 1: 사용자가 시작 버튼을 누르면 질문과 보기 데이터를 요청
+
+
+    // TODO 2: 사용자가 선택한 보기 값을 Service에 전달하여 저장 요청
+
+
+    // TODO 3: city_name과 user_name을 사용자에게 보여주기
+
+
+    // TODO 4: Home 버튼을 누르면 메인 페이지로 리다이렉트
+}
