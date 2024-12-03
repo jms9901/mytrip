f389c4732b24b7df5eb6cec10e3484b3d89a0870
answer0424 <wonwon123123@naver.com>
2024-12-03 11:40:55 +0900

    feat: userRepository mapper 파일 및 인터페이스 기능 추가

--- src/main/java/com/lec/spring/mytrip/repository/UserRepository.java
+++ src/main/java/com/lec/spring/mytrip/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.lec.spring.mytrip.repository;
 
-public class UserRepository {
+import com.lec.spring.mytrip.domain.User;
+
+public interface UserRepository {
+    //특정 id를 가진 user 리턴
+    User findById(Long id);
+
+    // 특정  username 의 user 리턴
+    User findbyUsername(String username);
+
+    // 새로운 user 등록
+    int saveUser (User user);
 }
