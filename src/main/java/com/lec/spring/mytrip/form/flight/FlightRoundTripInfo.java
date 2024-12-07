71ea34e9aed64a2f9d7c205f59e6a7153e912877
MoonD <wns50511@gmail.com>
2024-12-07 21:56:05 +0900

    feat: API 호출 단계 3

--- src/main/java/com/lec/spring/mytrip/form/flight/FlightRoundTripInfo.java
+++ src/main/java/com/lec/spring/mytrip/form/flight/FlightRoundTripInfo.java
@@ -7,7 +7,19 @@ public class FlightRoundTripInfo {
     //화면에 출력할 값 목록
     String id;          // 항공 id, 디테일에 필요
     String price;       // 가격
+    String totalResults; // 총 결과 검색 수
     String departure;   // 출발일시
+    String durationInMinutes; //  걸리는 시간(분)
+    String arrival; // 도착시간
+    String originDisplayCode; // 출발공항코드 ex) ICN
+    String destinationDisplayCode; // 도착공항코드 ex) ICN
+
+
+    String originName; // 출발 공항 이름
+    String destinationName; // 도착 공항 이름
+
+
+
 
     String token;       // 디테일 검색에 필요한 토큰 값
     String sessionId;   // 인컴플 호출에 필요한 세션id
