b30b8c014555dc18b549fddf2da82c097732a8a4
0105moonsun <0105moonsun@gmail.com>
2024-12-08 12:14:01 +0900

    feat:admin 임시파일

---
+++ src/main/resources/static/JS/adminLogin.js
@@ -0,0 +1,5 @@
+window.onload = async function () {
+    document.body.style.backgroundImage = "url('/img/admin/AdminLoginImg.jpg')";
+    document.body.style.backgroundSize = "cover"; // 이미지 크기 조정
+    document.body.style.backgroundPosition = "center"; // 이미지 위치
+};
