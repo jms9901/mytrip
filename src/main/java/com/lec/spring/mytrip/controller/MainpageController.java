c31f42dc49768d1defe9c6b6f6d8499ada482076
answer0424 <wonwon123123@naver.com>
2024-12-09 16:29:35 +0900

    feat: mainpage.html package top10 게시물 데이터 불러오기 -완-

--- src/main/java/com/lec/spring/mytrip/controller/MainpageController.java
+++ src/main/java/com/lec/spring/mytrip/controller/MainpageController.java
@@ -23,18 +23,26 @@ public class MainpageController {
         this.mainpageService = mainpageService;
     }
 
-    // TODO : 팝업    --> boardpage
-    // 좋아요가 가장 많은 여행지 정보를 Service에게 요청한다.
 
     // 사용자가 클릭한 값(사진/X)에 따라 다른 Controller에게 요청하거나 팝업을 닫는 동작을 처리한다.
     @GetMapping("/mainpage")
-    public String mainpage() {
+    public String mainpage(Model model) {
+        // 서비스 계층에서 상위 2개의 추천 도시 데이터를 가져오기
+        List<City> mostRecommendedCities = mainpageService.getMostRecommendedCities();
+        model.addAttribute("mostRecommendedCities", mostRecommendedCities);
+
+        // 가장 최신에 등록된 10개의 패키지 게시물 가져오기
+        List<Package> latestPackages = mainpageService.getLatestPackages();
+        model.addAttribute("latestPackages", latestPackages);
+
         return "main/mainpage";
     }
 
     // ai page로 이동
     @GetMapping("/aipage/start")
-    public void aipageStart() {}
+    public String aipageStart() {
+        return "aipage/start";
+    }
 
     // login page로 이동
     @GetMapping("/user/login")
@@ -48,23 +56,7 @@ public class MainpageController {
     @GetMapping("/flight/search")
     public void flightSearch() {}
 
-    // TODO : AI 추천여행지   --> aipage
-    @GetMapping("/ai-recommendations")
-    public String showRecommendedCities(Model model) {
-        // 서비스 계층에서 상위 2개의 추천 도시 데이터를 가져오기
-        List<City> mostRecommendedCities = mainpageService.getMostRecommendedCities();
-        model.addAttribute("mostRecommendedCities", mostRecommendedCities);
-        return "ai-recommendations"; // View 이름
-    }
 
-    // TODO : 투어패키지     --> boardpage
-    // 사용자가 투어패키지 정보를 볼 수 있게 Service에게 요청한다.
-    // 사용자가 투어패키지를 클릭하면, BoardController에게 해당 투어 페이지로 이동하도록 요청을 전달한다.
-    @GetMapping("/package-lastest")
-    public String showPackageLastest(Model model) {
-        List<Package> latestPackages = mainpageService.getLatestPackages();
-        model.addAttribute("latestPackages", latestPackages);
-        return "package-lastest";
-    }
-
+    // TODO : 팝업    --> boardpage
+    // 좋아요가 가장 많은 여행지 정보를 Service에게 요청한다.
 }
