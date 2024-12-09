b6291dd9f5ada41c03dff9949d0869cc9a0ebe29
sihooh <khy2001smy@gmail.com>
2024-12-09 12:29:01 +0900

    feat: 첨부파일 repository 내용 추가

--- src/main/java/com/lec/spring/mytrip/repository/PostAttachmentRepository.java
+++ src/main/java/com/lec/spring/mytrip/repository/PostAttachmentRepository.java
@@ -1,7 +1,22 @@
 package com.lec.spring.mytrip.repository;
 
+import com.lec.spring.mytrip.domain.PostAttachment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
 public interface PostAttachmentRepository {
+    // 첨부파일 등록
+    int save(PostAttachment attachment);
 
-    // 파일
+    // 특정 피드의 첨부파일 목록 조회
+    List<PostAttachment> findAttachmentsByBoardId(@Param("boardId") Long boardId);
 
+    // 특정 피드의 모든 첨부파일 삭제
+    int deleteAttachmentsByBoardId(@Param("boardId") Long boardId);
+
+    // 첨부파일 개별 삭제
+    int deleteAttachmentById(@Param("attachmentId") Long attachmentId);
 }
