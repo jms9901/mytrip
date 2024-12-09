43450dfb7458e76804a25c3efc44a56f4789bdec
MoonD <wns50511@gmail.com>
2024-12-09 12:45:17 +0900

    feat: Domain 구현

---
+++ src/main/java/com/lec/spring/mytrip/domain/attachment/PackageAttachment.java
@@ -0,0 +1,16 @@
+package com.lec.spring.mytrip.domain.attachment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PackageAttachment {
+    private Long packageAttachmentId; // 첨부파일 ID
+    private Long packageId;           // 패키지 ID
+    private String fileName;          // 첨부파일 이름'
+}
