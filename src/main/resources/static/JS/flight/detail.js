102e09b0256a39afc2d7c3b9e3c6c0ff8ac2e235
answer0424 <wonwon123123@naver.com>
2024-12-14 11:09:34 +0900

    feat: board/city css 수정

--- src/main/resources/static/JS/flight/detail.js
+++ src/main/resources/static/JS/flight/detail.js
@@ -18,3 +18,38 @@ window.addEventListener('scroll', () => {
         scrollIndicator.classList.remove('hidden');
     }
 });
+
+$(document).ready(function  (event) {
+
+        console.log("board details button clicked:", event.target);
+
+        // 데이터 가져오기
+        const cityImg = event.target.getAttribute('data-cityImg');
+
+        console.log("Fetching attachments for boardId:", cityImg);
+
+        // 첨부파일 가져오기
+        fetch(`/flight/detailImg/{cityName}`)
+            .then((response) => response.json())
+            .then((cityImg) => {
+                const imageContainer = document.getElementById('imageContainer');
+                imageContainer.innerHTML = ''; // 기존 이미지 제거
+
+                if (cityImg && cityImg.length > 0) {
+                    cityImg.forEach((cityImg) => {
+                        const img = document.createElement('img');
+                        img.src = `/img/${cityImg.cityName}`; // 서버의 업로드 디렉토리 경로
+                        img.alt = '첨부 이미지';
+                        imageContainer.appendChild(img);
+                    });
+                } else {
+                    imageContainer.innerHTML = '<p>첨부파일이 없습니다.</p>';
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching attachments:", error);
+                alert("첨부파일 정보를 불러오지 못했습니다.");
+            });
+
+
+});
