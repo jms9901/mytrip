584604c1544ba9e87b7767d189e88778d5827b50
Minhyeok-Create <drawingself7@gmail.com>
2024-12-17 09:09:12 +0900

    feat: 마이페이지 & 메인페이지 머지 완료

--- src/main/resources/static/js/mainsection2.js
+++ src/main/resources/static/js/mainsection2.js
@@ -14,4 +14,22 @@ document.addEventListener("DOMContentLoaded", function() {
         slide.addEventListener('mouseover', stopSlide); // 마우스 오버 시 슬라이드 멈춤
         slide.addEventListener('mouseout', startSlide); // 마우스 아웃 시 슬라이드 재개
     });
+    document.querySelector('.mypage').addEventListener('click', () => {
+        fetch('../../mypage/loginuserid')
+            .then(response => response.json())
+            .then(data => {
+                const loguser = data.userId;
+                if (loguser) {
+                    window.location.href = `/mypage/${loguser}`;
+                } else {
+                    alert("로그인된 사용자를 찾을 수 없습니다.");
+                }
+            })
+            .catch(error => {
+                console.error('에러 발생:', error);
+                alert("로그인 정보를 가져오는 데 실패했습니다.");
+            });
+
+
+});
 });
